6db91cd999362b6de11247dbcbec4aeae7a995d1
周文洋 <zhouwenyang@it.local>
2023-12-14 20:35:17 +0800

    add sync

---
+++ 00-run_repo_sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3.9 01-analysis-super-manifest.py
+python3.9 02-analysis-submodules.py
+python3.9 03-sync-repos.py
+python3.9 04-sync-submodules.py
+python3.9 05-sync-mannual-repos.py
