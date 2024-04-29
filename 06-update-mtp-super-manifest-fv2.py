df783ddb8d1fa76b40e481e9be921f9813d1afc7
JackyZhou <info@zhouwenyang.com>
2024-04-29 10:51:50 +0800

    Update 06-update-mtp-super-manifest-fv2.py

--- 06-update-mtp-super-manifest-fv2.py
+++ 06-update-mtp-super-manifest-fv2.py
@@ -1,5 +1,8 @@
 import requests
 from xml.etree import ElementTree as ET
+import subprocess
+import os
+import shutil
 
 # 下载XML文件
 url = "https://raw.githubusercontent.com/Infineon/mtb-super-manifest/v2.X/mtb-super-manifest-fv2.xml"
@@ -11,7 +14,6 @@ root = ET.fromstring(xml_content)
 
 # 遍历所有<uri>节点并根据条件删除
 prefix = "https://github.com/Infineon"
-# 因为我们正在删除节点，所以我们需要遍历所有的父节点
 for parent in root.findall(".//board-manifest"):
     uris = parent.findall("uri")
     for uri in uris:
@@ -32,8 +34,31 @@ for parent in root.findall(".//middleware-manifest"):
 
 # 将修改后的XML写入新文件
 tree = ET.ElementTree(root)
-with open("mtb-super-manifest-fv2-mirror.xml", "wb") as modified_file:
-    # 写入文件时不包括XML声明，并确保使用正确的编码
+modified_xml_path = "mtb-super-manifest-fv2-mirror.xml"
+with open(modified_xml_path, "wb") as modified_file:
     tree.write(modified_file, encoding='utf-8', xml_declaration=False)
 
-print("修改后的XML文件已保存为 'mtb-super-manifest-fv2-mirror.xml'")
+print("修改后的XML文件已保存")
+
+# Git 操作
+repo_url = "https://mtbgit.infineon.cn/Infineon/mtb-super-manifest"
+repo_path = "mtb-super-manifest"
+
+# 检查目录是否存在，如果存在则删除
+if os.path.exists(repo_path):
+    shutil.rmtree(repo_path)
+
+# 克隆仓库
+subprocess.run(["git", "clone", repo_url], check=True)
+
+# 复制文件到仓库目录
+destination_file_path = os.path.join(repo_path, modified_xml_path)
+os.replace(modified_xml_path, destination_file_path)
+
+# Git 添加、提交和推送
+os.chdir(repo_path)
+subprocess.run(["git", "add", modified_xml_path], check=True)
+subprocess.run(["git", "commit", "-m", "Update mtb-super-manifest-fv2-mirror.xml"], check=True)
+subprocess.run(["git", "push"], check=True)
+
+print("Git操作完成，文件已更新和推送到远程仓库")
